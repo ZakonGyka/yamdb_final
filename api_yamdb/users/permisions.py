4e415fb23d32431a0b65afc67e82da9808aa9c1f
zakongyka <zakongyka@yandex.ru>
2022-10-31 18:13:40 +0100

    fix elif to if in users/permisions

--- api_yamdb/users/permisions.py
+++ api_yamdb/users/permisions.py
@@ -22,7 +22,7 @@ class IsAuthorOrAdminOrModerator(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.method == 'POST':
+        if request.method == 'POST':
             return request.user.is_authenticated
         return (request.user.is_authenticated
                 and (request.user.is_admin
