61a9446c8c9bb27b6a070f63ebe7a74d6dcde57f
zakongyka <zakongyka@yandex.ru>
2022-11-01 20:55:26 +0100

    pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import datetime
+import os
 
 from dotenv import load_dotenv  # for python-dotenv method
 
 load_dotenv()  # for python-dotenv method
-
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
